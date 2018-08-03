32f5b4e8bcb844e97b735e1f96633477ba0f0dd5
kamranash <kamranash@local>
2018-08-03 21:32:40 +0000

    Lab 1 work v3

--- lab1/text_recognizer/networks/mlp.py
+++ lab1/text_recognizer/networks/mlp.py
@@ -7,7 +7,7 @@ from tensorflow.keras.layers import Dense, Dropout, Flatten
 def mlp(input_shape: Tuple[int, ...],
         output_shape: Tuple[int, ...],
         layer_size: int=128,
-        dropout_amount: float=0.1,
+        dropout_amount: float=0.2,
         num_layers: int=3) -> Model:
     """
     Simple multi-layer perceptron: just fully-connected layers with dropout between them, with softmax predictions.
@@ -18,11 +18,17 @@ def mlp(input_shape: Tuple[int, ...],
     model = Sequential()
     # Don't forget to pass input_shape to the first layer of the model
     ##### Your code below (Lab 1)
-    model.add(Flatten(input_shape=input_shape))
-    for _ in range(num_layers):
-        model.add(Dense(layer_size, activation='relu'))
-        model.add(Dropout(dropout_amount))
-        model.add(Dense(layer_size, activation='relu'))
+    model.add(Flatten(input_shape=input_shape))  
+    #model.add(Dense(input_shape[0]*input_shape[0], activation='relu'))
+    model.add(Dense(input_shape[0]*input_shape[0], activation='relu'))
+    model.add(Dropout(dropout_amount))
+    model.add(Dense((input_shape[0]*input_shape[0])/2, activation='relu'))
+    model.add(Dense(layer_size, activation='relu'))
+#    model.add(Dense(layer_size/2, activation='relu'))
+    
+#    for _ in range(num_layers):
+#        model.add(Dense(layer_size, activation='relu'))
+#        model.add(Dropout(dropout_amount))
     model.add(Dense(num_classes, activation='softmax'))
     ##### Your code above (Lab 1)
 
