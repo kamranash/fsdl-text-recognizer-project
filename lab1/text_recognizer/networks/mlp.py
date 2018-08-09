4340e7d28c5176f9203cb1bb0b935d08a57ee32d
kamranash <kamranash@local>
2018-08-09 22:07:50 +0000

    v4

--- lab1/text_recognizer/networks/mlp.py
+++ lab1/text_recognizer/networks/mlp.py
@@ -25,9 +25,11 @@ def mlp(input_shape: Tuple[int, ...],
  
     #input_shape = np.reshape(input_shape, (input_shape[0], input_shape[1], 1))
     #model.add(Dense((input_shape[0]*input_shape[1]), activation='relu'))
-    model.add(Conv2D(32, (4, 4), strides = (1, 1), padding='same', activation='relu' ))
+    model.add(Conv2D(64, (4, 4), strides = (1, 1), padding='same', activation='relu' ))
     #model.add(MaxPooling2D((2, 2)))
     model.add(Conv2D(32, (4, 4), strides = (1, 1), padding='same', activation='relu' ))
+    #model.add(Conv2D(32, (4, 4), strides = (1, 1), padding='same', activation='relu' ))
+    
     #model.add(Conv2D(8, (2, 2), strides = (1, 1), activation='relu' ))
     #model.add(Conv2D(4, (2, 2), strides = (1, 1), activation='relu' ))
     model.add(MaxPooling2D((2, 2)))
@@ -37,9 +39,11 @@ def mlp(input_shape: Tuple[int, ...],
     #model.add(Dropout(dropout_amount))
     #model.add(Dense((input_shape[0]*input_shape[0])/2, activation='relu'))
     model.add(Flatten())
+    model.add(Dropout(dropout_amount))
     model.add(Dense(layer_size, activation='relu'))
-    Dropout(dropout_amount)
+    
     model.add(Dense(layer_size, activation='relu'))
+    
     #model.add(Dense(layer_size/2, activation='relu'))
     
 #    for _ in range(num_layers):
